ebd99d81491314b76fd562a86fac2ee492557afd
Tohirjon <tohirjonxatamov0924@gmail.com>
2025-07-24 08:35:02 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,169 @@
+// 1-masala
+
+// const { use } = require("react")
+
+// let getUserInfo=(user)=>{
+
+//     console.log(`Foydalanuvchi ismi: ${user["name"]}, yoshi: ${user["age"]}`);
+
+// }
+// let user={
+//     name:"Ali",
+//     age:25,
+// }
+
+// getUserInfo(user)
+
+// 2-masala
+
+// let addTax = (product) => {
+//   product.priceWithTax = product.price * 1.12;
+//   console.log(product);
+// };
+// let product = {
+//   name: "Telefon",
+//   price: 1000,
+// };
+// addTax(product);
+
+// 4-masala
+
+// let carInfo = (car) => {
+//     console.log(`Bu ${car.color} rangli ${car.model}, ${car.year}-yil`);
+
+// };
+
+// let car = {
+//   model: "Cobalt",
+//   year: 2022,
+//   color: "oq",
+// };
+
+// carInfo(car)
+
+// 5-masala
+
+// const hasEmail=(user, key)=>{
+// if(key in user) return true;
+// return false
+// }
+// const user={
+//     name:"Ali",
+//     email:"ali@gmail.com"
+// }
+
+// console.log(hasEmail(user, "email"));
+
+// 6-masala
+
+// const getTotalSalary = (employees) => {
+
+// };
+
+// let salary=[
+//     {
+//         name:"Ali",
+//         salary:1000
+//     },
+//     {
+//         name:"Vali",
+//         salary:1500
+//     }
+// ]
+
+// 6-masala
+
+// let calculateSpeed = (car) => {
+//   car.speed = 50;
+//   console.log(car);
+// };
+
+// let car = {
+//   model: "Cobalt",
+//   distance: 150,
+//   time: 3,
+// };
+
+// calculateSpeed(car)
+
+// 7-masala
+
+// let calculateWorkTime = (worker) => {
+//   worker.hoursWorked = (worker.end - worker.start);
+//   console.log(worker);
+// };
+
+// let worker={
+//     name:"Ali",
+//     start:9,
+//     end:18,
+// };
+
+// calculateWorkTime(worker)
+
+// 8-masal
+
+// let calculateVolume=(cube)=>{
+// cube.volume=Math.pow(cube.side,3)
+
+// console.log(cube);
+
+// };
+
+// let cube={
+//     side:4,
+// };
+
+// calculateVolume(cube)
+
+// 9-masala
+
+// let isPasswordStrong=(user)=>{
+//     user.isStrong=user.password.toString().length>=8;
+//     console.log(user);
+    
+// };
+
+// let user={
+//     username:"test",
+//     password:12345678,
+
+// };
+
+// isPasswordStrong(user)
+
+// 10-masala
+
+// let calculaeBirthYear=(person)=>{
+//     person.birthYear=2025-person.age;
+//     console.log(person);
+    
+// };
+
+// let person={
+//     name:"Laylo",
+//     age:20,
+// };
+
+// calculaeBirthYear(person);
+
+
+// 5-masala;
+
+// let checkProperty=(obj, key)=>{
+// if(key in obj)return true; return false
+
+// }
+
+// let book={
+//     title:"JS Fundamentals",
+//     author:"Mirzo"
+// }
+
+// let check=checkProperty(book, "author")
+// console.log(check);
+
+
+
+
+
